aada86a9a32a778418bc5a9c3e558e4c3a1caa87
Sun Xiaofei <adoni1203@gmail.com>
2015-01-15 14:41:02 +0800

    finish time calculate

--- time_of_us/views.py
+++ time_of_us/views.py
@@ -2,16 +2,25 @@ from django.shortcuts import render_to_response
 from adoni.settings import TIME_ZONE
 import os
 import time
+import datetime
+
 
 def time_of_us(request):
     os.environ["TZ"] = TIME_ZONE
     time.tzset()
+    d1 = datetime.datetime.now()
+    d2 = datetime.datetime(2013,5,30,0,0,0)
+    delta_time = d1-d2
+    day = delta_time.days
+    hour = delta_time.seconds/3600
+    minute = (delta_time.seconds % 3600)/60
+    second = delta_time.seconds % 60
     return render_to_response(
         'welcome.html',
         {
-            'day':100,
-            'hour':10,
-            'minute':30,
-            'second':10
+            'day':day,
+            'hour':hour,
+            'minute':minute,
+            'second':second
         }
     )
